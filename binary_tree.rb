3a3782a25e4002d9c935ba1e04c847317379a4ae
Nicholas Manos <manos.nick@gmail.com>
2014-11-13 13:07:53 -0800

    rubocop fixes

--- binary_tree.rb
+++ binary_tree.rb
@@ -1,100 +1,85 @@
+# A binary tree with 3 different traversal methods.
 class BinaryTree
-
-  def initialize()
+  def initialize
     @root = Node.new
   end
 
   attr_accessor :root
 
-  def preOrder(node)
+  def pre_order(node)
     p node.data
-    if node.left != nil
-      preOrder(node.left)
-    end
-    if node.right != nil
-      preOrder(node.right)
-    end
+    pre_order(node.left) unless node.left.nil?
+    pre_order(node.right) unless node.right.nil?
   end
 
-  def inOrder(node)
-    if node.left != nil
-      inOrder(node.left)
-    end
+  def in_order(node)
+    in_order(node.left) unless node.left.nil?
     p node.data
-    if node.right != nil
-      inOrder(node.right)
-    end
+    in_order(node.right) unless node.right.nil?
   end
 
-  def postOrder(node)
-    if node.left != nil
-      postOrder(node.left)
-    end
-    if node.right != nil
-      postOrder(node.right)
-    end
+  def post_order(node)
+    post_order(node.left) unless node.left.nil?
+    post_order(node.right) unless node.right.nil?
     p node.data
   end
 
   def traverse
     print "Pre-Order Traversal of tree\n"
-    preOrder(@root)
+    pre_order(@root)
 
     print "In-Order Traversal of tree\n"
-    inOrder(@root)
+    in_order(@root)
 
     print "Post-Order Traversal of tree\n"
-    postOrder(@root)
+    post_order(@root)
   end
-
 end
-
+# A Node for the binary tree
 class Node
-
-  def initialize()
+  def initialize
     @left = nil
     @right = nil
     @data = nil
   end
 
   attr_accessor :left, :right, :data
-
 end
 
 tree = BinaryTree.new
 root = tree.root
-root.data = "Tim"
+root.data = 'Tim'
 
 node = Node.new
-node.data = "Phil"
+node.data = 'Phil'
 root.right = node
 
 node = Node.new
-node.data = "Craig"
+node.data = 'Craig'
 root.right.right = node
 
 node = Node.new
-node.data = "Eddie"
+node.data = 'Eddie'
 root.right.left = node
 
 node = Node.new
-node.data = "Jony"
+node.data = 'Jony'
 root.left = node
 
 node = Node.new
-node.data = "Dan"
+node.data = 'Dan'
 root.left.left = node
 
 node = Node.new
-node.data = "Katie"
+node.data = 'Katie'
 root.left.right = node
 
 node = Node.new
-node.data = "Andrea"
+node.data = 'Andrea'
 root.left.right.left = node
 
 node = Node.new
-node.data = "Peter"
+node.data = 'Peter'
 root.left.right.right = node
 
 tree.traverse
